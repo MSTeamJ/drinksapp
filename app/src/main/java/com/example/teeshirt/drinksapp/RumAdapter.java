115fac39507750968257d846812e9849536ab49e
owuor91 <owuor91@gmail.com>
2015-05-27 12:20:32 +0300

    add Tequilla adapter

---
+++ app/src/main/java/com/example/teeshirt/drinksapp/RumAdapter.java
@@ -0,0 +1,49 @@
+package com.example.teeshirt.drinksapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+/**
+ * Created by teeshirt on 5/27/15.
+ */
+public class RumAdapter extends ParseQueryAdapter<Drink> {
+    public RumAdapter(Context context){
+        super(context, new ParseQueryAdapter.QueryFactory<Drink>(){
+            public ParseQuery<Drink> create(){
+                ParseQuery query = new ParseQuery("Drink");
+                query.whereEqualTo("category","Liqueur");
+                return query;
+            }
+        });
+    }
+
+    @Override
+    public View getItemView(Drink drink, View v, ViewGroup parent) {
+        if (v==null){
+            v = View.inflate(getContext(),R.layout.rowview,null);
+        }
+
+        super.getItemView(drink, v, parent);
+
+        ParseImageView icon = (ParseImageView) v.findViewById(android.R.id.icon);
+        ParseFile imageFile = drink.getParseFile("image");
+
+        if (imageFile!=null){
+            icon.setParseFile(imageFile);
+            icon.loadInBackground();
+        }
+
+        TextView text1 = (TextView) v.findViewById(R.id.text1);
+        text1.setText(drink.getString("name"));
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/teeshirt/drinksapp/TequillaAdapter.java
@@ -0,0 +1,9 @@
+package com.example.teeshirt.drinksapp;
+
+import com.parse.ParseQueryAdapter;
+
+/**
+ * Created by teeshirt on 5/27/15.
+ */
+public class TequillaAdapter extends ParseQueryAdapter<Drink> {
+}
