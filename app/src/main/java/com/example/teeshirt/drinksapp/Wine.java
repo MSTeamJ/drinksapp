5dbbbed71b0de795a2a15794b3a260e165a50e3d
owuor91 <owuor91@gmail.com>
2015-05-27 12:38:34 +0300

    Add wineadapter

--- app/src/main/java/com/example/teeshirt/drinksapp/Wine.java
+++ app/src/main/java/com/example/teeshirt/drinksapp/Wine.java
@@ -1,38 +1,26 @@
 package com.example.teeshirt.drinksapp;
 
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Wine extends ActionBarActivity {
+public class Wine extends ListActivity {
 
+    private WineAdapter wineAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine);
+
+        wineAdapter = new WineAdapter(this);
+        wineAdapter.setImageKey("image");
+        wineAdapter.setTextKey("name");
+
+        setListAdapter(wineAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wine, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
