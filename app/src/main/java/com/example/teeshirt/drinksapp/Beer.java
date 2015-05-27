85517838fe16c1de7d809b5a2dcede7870c60fdf
owuor91 <owuor91@gmail.com>
2015-05-27 13:27:39 +0300

    Details activity layout created

--- app/src/main/java/com/example/teeshirt/drinksapp/Beer.java
+++ app/src/main/java/com/example/teeshirt/drinksapp/Beer.java
@@ -2,42 +2,33 @@ package com.example.teeshirt.drinksapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class Beer extends ListActivity {
 
     private BeerAdapter beerAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer);
-
-
-
 
         beerAdapter = new BeerAdapter(this);
         beerAdapter.setTextKey("name");
         beerAdapter.setImageKey("image");
 
         setListAdapter(beerAdapter);
-
     }
 
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
 }
 
-
-/*
-final ParseObject beer = new ParseObject("Drink");
-ParseQuery<Drink> query = ParseQuery.getQuery("Drink");
-query.whereEqualTo("category","beer");
-        query.findInBackground(new FindCallback<Drink>() {
-@Override
-public void done(List<Drink> list, ParseException e) {
-        if (e==null){
-        String name= beer.getString("name");
-
-        }
-        }
-        });*/
