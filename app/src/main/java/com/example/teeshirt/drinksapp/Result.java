ea9ea001600fb21cae0189933774a6a3bef9dd88
owuor91 <owuor91@gmail.com>
2015-05-29 02:00:05 +0300

    on click listener for results list

--- app/src/main/java/com/example/teeshirt/drinksapp/Result.java
+++ app/src/main/java/com/example/teeshirt/drinksapp/Result.java
@@ -1,10 +1,14 @@
 package com.example.teeshirt.drinksapp;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -28,6 +32,23 @@ public class Result extends ListActivity {
         resultAdapter.setImageKey("image");
 
         setListAdapter(resultAdapter);
+
+
+        final ListView listView = (ListView)findViewById(android.R.id.list);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Object val = listView.getItemAtPosition(position);
+                        Drink selected = (Drink) val;
+                        String nom = selected.getName();
+
+                        Intent intent = new Intent(getApplicationContext(), Details.class);
+                        intent.putExtra("drinkname", nom);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
     @Override
