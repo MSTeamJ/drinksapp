2e49e70e0cbc1b4dbbf2d1b547b28053f15bd830
owuor91 <owuor91@gmail.com>
2015-05-29 01:19:16 +0300

    Pass serch term to search result activity

--- app/src/main/java/com/example/teeshirt/drinksapp/Search.java
+++ app/src/main/java/com/example/teeshirt/drinksapp/Search.java
@@ -1,10 +1,12 @@
 package com.example.teeshirt.drinksapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,10 +20,23 @@ public class Search extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        etSearch = (EditText) findViewById(R.id.etSearch);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+
         Typeface font = Typeface.createFromAsset(getAssets(), "raleway.ttf");
         etSearch.setTypeface(font);
         btnSearch.setTypeface(font);
-        
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchTerm = etSearch.getText().toString();
+                Intent intent = new Intent(Search.this, Result.class);
+                intent.putExtra("term", searchTerm);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
