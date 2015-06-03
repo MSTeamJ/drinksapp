de0c430a9a2f20af43fb63d9c79c86806f4698a3
owuor91 <owuor91@gmail.com>
2015-06-03 21:37:28 +0300

    Cast layout views and get Selected radiobutton value

--- app/src/main/java/com/example/teeshirt/drinksapp/Aroma.java
+++ app/src/main/java/com/example/teeshirt/drinksapp/Aroma.java
@@ -1,11 +1,15 @@
 package com.example.teeshirt.drinksapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,6 +18,8 @@ public class Aroma extends ActionBarActivity {
     private RadioButton radio, floral, citrus, woody, malty;
     public Bundle taste;
     public String drinkTaste;
+    public Button aromaBtn;
+    public TextView txtAroma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,35 @@ public class Aroma extends ActionBarActivity {
         if (taste!=null){
             drinkTaste = taste.getString("taste");
             Toast.makeText(Aroma.this, drinkTaste, Toast.LENGTH_SHORT).show();
+
+            txtAroma = (TextView) findViewById(R.id.txtAroma);
+
+            floral = (RadioButton) findViewById(R.id.floral);
+            woody = (RadioButton) findViewById(R.id.woody);
+            citrus = (RadioButton) findViewById(R.id.citrus);
+            malty = (RadioButton) findViewById(R.id.malty);
+
+            aromaGroup = (RadioGroup) findViewById(R.id.aromaGroup);
+            aromaBtn = (Button) findViewById(R.id.aromaBtn);
+
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "raleway.ttf");
+            floral.setTypeface(typeface);
+            woody.setTypeface(typeface);
+            citrus.setTypeface(typeface);
+            malty.setTypeface(typeface);
+            txtAroma.setTypeface(typeface);
+            aromaBtn.setTypeface(typeface);
+
+            aromaBtn.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int selected = aromaGroup.getCheckedRadioButtonId();
+                            radio = (RadioButton) findViewById(selected);
+                            Toast.makeText(Aroma.this, radio.getText().toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
         }
 
     }
